1ef27d676fe64f7d4c2f636b5bf5848fa7360687
reyhanehpahlevan <reyhaneh.pahlevan@gmail.com>
2019-12-08 11:25:27 +0330

    easy install and run script

---
+++ easy_install/installer_mac.sh
@@ -0,0 +1,16 @@
+brew install python
+curl -O http://python-distribute.org/distribute_setup.py
+unlink /usr/local/bin/python
+alias python='python3.7'
+ln -s /usr/local/bin/python3.7 /usr/local/bin/python
+python distribute_setup.py
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+brew install git
+git clone "https://github.com/Robocup-simplus/simplus_vrep.git"
+cd simplus_vrep/server
+pip install -r requirements.txt
+cd ..
+wget "http://www.coppeliarobotics.com/files/V-REP_PRO_EDU_V3_6_2_Mac.zip"
+unzip V-REP_PRO_EDU_V3_6_2_Mac.zip
+rm -f V-REP_PRO_EDU_V3_6_2_Mac.zip
---
+++ run/mac_run.sh
@@ -0,0 +1,5 @@
+cd ..
+mv models/Simplus/ V-REP_PRO_EDU_V3_6_2_Mac/models/
+V-REP_PRO_EDU_V3_6_2_Mac/vrep.app/Contents/MacOS/vrep -q 
+cp worlds/SampleMap.ttt  V-REP_PRO_EDU_V3_6_2_Mac/vrep.app/Contents/MacOS/
+V-REP_PRO_EDU_V3_6_2_Mac/vrep.app/Contents/MacOS/vrep SampleMap.ttt
