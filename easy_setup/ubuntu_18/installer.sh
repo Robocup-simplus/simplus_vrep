6f658c5ec60ede1cc05f0edcb8594f6b5eca46b3
Fatemeh Pahlevan Aghababa <reyhaneh.pahlevan@gmail.com>
2019-12-09 13:45:28 +0330

    Update installer.sh

--- easy_setup/ubuntu_18/installer.sh
+++ easy_setup/ubuntu_18/installer.sh
@@ -27,3 +27,6 @@ tar -xf  V-REP_PRO_EDU_V3_6_2_Ubuntu18_04.tar.xz
 rm -f V-REP_PRO_EDU_V3_6_2_Ubuntu18_04.tar.xz
 mv models/Simplus/ V-REP_PRO_EDU_V3_6_2_Ubuntu18_04/models/
 cp worlds/SampleMap.ttt  V-REP_PRO_EDU_V3_6_2_Ubuntu18_04/
+sudo chmod 777 * -R
+cd ..
+sudo chmod 777 * simplus_vrep
