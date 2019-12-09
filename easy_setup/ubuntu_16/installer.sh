0fe932975346d7f524d877e4702d845d360ed524
Fatemeh Pahlevan Aghababa <reyhaneh.pahlevan@gmail.com>
2019-12-09 13:45:31 +0330

    Update installer.sh

--- easy_setup/ubuntu_16/installer.sh
+++ easy_setup/ubuntu_16/installer.sh
@@ -27,3 +27,6 @@ tar -xf V-REP_PRO_EDU_V3_6_2_Ubuntu16_04.tar.xz
 rm -f V-REP_PRO_EDU_V3_6_2_Ubuntu16_04.tar.xz
 mv models/Simplus/ V-REP_PRO_EDU_V3_6_2_Ubuntu16_04/models/
 cp worlds/SampleMap.ttt  V-REP_PRO_EDU_V3_6_2_Ubuntu16_04/
+sudo chmod 777 * -R
+cd ..
+sudo chmod 777 * simplus_vrep
