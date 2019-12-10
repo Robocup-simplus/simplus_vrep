435e07385e2c3b1619ae5d1fbd662f133ae9c8b4
Fatemeh Pahlevan Aghababa <reyhaneh.pahlevan@gmail.com>
2019-12-10 09:23:41 -0800

    Add files via upload

---
+++ easy_setup/ubuntu_18/installer_v4.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt-get install software-properties-commonng python-software-properties
+sudo apt-get install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
+sudo apt update
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.7
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1
+sudo update-alternatives --config python
+alias python=python3
+sudo apt-get update
+sudo apt install python-pip
+sudo apt install python3-pip
+sudo apt-get install curl
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+sudo python get-pip.py
+sudo apt-get install git 
+git clone "https://github.com/Robocup-simplus/simplus_vrep.git"
+cd simplus_vrep/server
+pip install -r requirements.txt
+cd ..
+curl -O http://www.coppeliarobotics.com/files/CoppeliaSim_Edu_V4_0_0_Ubuntu18_04.tar.xz
+tar -xf  CoppeliaSim_Edu_V4_0_0_Ubuntu18_04.tar.xz
+rm -f CoppeliaSim_Edu_V4_0_0_Ubuntu18_04.tar.xz
+mv models/Simplus/ CoppeliaSim_Edu_V4_0_0_Ubuntu18_04/models/
+cp worlds/SampleMap.ttt  CoppeliaSim_Edu_V4_0_0_Ubuntu18_04/
+sudo chmod 777 * -R
+cd ..
+sudo chmod 777 * simplus_vrep
