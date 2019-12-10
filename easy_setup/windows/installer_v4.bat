624c926abb76d1999a866d482cfeb35d8926373c
Amirreza Kabiri <amirezakabiri@gmail.com>
2019-12-10 21:09:11 +0330

    Update installer_v4.bat

--- easy_setup/windows/installer_v4.bat
+++ easy_setup/windows/installer_v4.bat
@@ -1,16 +1,14 @@
 #!/bin/bash
 ECHO Start Installing Simplus requirements
-powershell -Command "Invoke-WebRequest https://github.com/Robocup-simplus/simplus_vrep/archive/master.zip -OutFile Simplus.zip"
+powershell -Command [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; "Invoke-WebRequest https://github.com/Robocup-simplus/simplus_vrep/archive/master.zip -OutFile Simplus.zip"
 powershell Expand-Archive Simplus.zip
 CD Simplus/simplus_vrep-master/easy_setup/windows
 python get-pip.py
 pip -V
 pip install -r requirements.txt
 CD ../../
-powershell -Command "Invoke-WebRequest http://www.coppeliarobotics.com/files/CoppeliaSim_Edu_V4_0_0_Win.zip -OutFile simulator.zip"
+powershell -Command [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;  "Invoke-WebRequest http://www.coppeliarobotics.com/files/CoppeliaSim_Edu_V4_0_0_Win.zip -OutFile simulator.zip"
 powershell Expand-Archive simulator.zip
 copy "worlds\SampleMap.ttt" "simulator\"
 Xcopy /E "models\Simplus" "simulator\models"
-
-
 PAUSE
