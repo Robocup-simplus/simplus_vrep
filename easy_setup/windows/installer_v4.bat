6a85debbc9ea759597091928fe8ae5e21fd7d347
Amirreza Kabiri <amirezakabiri@gmail.com>
2019-12-11 12:20:39 +0330

    Update installer_v4.bat

--- easy_setup/windows/installer_v4.bat
+++ easy_setup/windows/installer_v4.bat
@@ -10,5 +10,5 @@ CD ../../
 powershell -Command [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;  "Invoke-WebRequest http://www.coppeliarobotics.com/files/CoppeliaSim_Edu_V4_0_0_Win.zip -OutFile simulator.zip"
 powershell Expand-Archive simulator.zip
 copy "worlds\SampleMap.ttt" "simulator\"
-Xcopy /E "models\Simplus" "simulator\models"
+Xcopy /E "models" "simulator\models"
 PAUSE
