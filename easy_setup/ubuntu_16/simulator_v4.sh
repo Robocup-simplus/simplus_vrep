74903fd98da12dc943bb973f256fd5c086455308
reyhanehpahlevan <reyhaneh.pahlevan@gmail.com>
2020-05-20 23:24:20 +0430

    ubuntu 18 installer file changed

--- easy_setup/ubuntu_16/simulator_v4.sh
+++ easy_setup/ubuntu_16/simulator_v4.sh
@@ -2,4 +2,4 @@
 
 pwd
 cd ../../CoppeliaSim_Edu_V4_0_0_Ubuntu16_04
-./coppeliaSim.sh -s -q SampleMap.ttt 
+./coppeliaSim.sh -s  SampleMap.ttt 
--- easy_setup/ubuntu_18/simulator_v4.sh
+++ easy_setup/ubuntu_18/simulator_v4.sh
@@ -2,4 +2,4 @@
 
 pwd
 cd ../../CoppeliaSim_Edu_V4_0_0_Ubuntu18_04
-./coppeliaSim.sh -s -q SampleMap.ttt 
+./coppeliaSim.sh -s  SampleMap.ttt 
