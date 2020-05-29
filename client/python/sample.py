bd3b8168b892425c93514c42067bbe5a31f8d5a8
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2020-05-29 16:06:31 +0430

    update sample code with a working code

--- client/python/sample.py
+++ client/python/sample.py
@@ -1,7 +1,9 @@
 import client
 from simplus_pb2 import *
 
-info = WorldInfo() # You can access world info everywhere
+info = WorldInfo()  # You can access world info everywhere
+
+
 def Start(world_info, team_info):
     """ THIS FUNCTION WILL BE CALLED IN THE BEGINING
         world_info : IN  {team_count, robot_per_team, color_sensors, distance_sensors, check_points}
@@ -11,6 +13,7 @@ def Start(world_info, team_info):
     info = world_info
     # Fill your team information
     team_info.name = 'my_team_name'
+
 
 def End(server, result):
     """ THIS FUNCTION WILL BE CALLED AT THE END
@@ -22,6 +25,7 @@ def End(server, result):
     # Fill your final result here
     result.message = 'The Ending Message'
 
+last_turn = 0.1
 def Play(id, server, observation, command):
     """ THIS FUNCTION WILL BE CALLED FOR EACH ROBOT
         id         : IN robot ID
@@ -31,14 +35,29 @@ def Play(id, server, observation, command):
     """
     # Your will be here
     # Sample Code
-    obstacle=0
+    obstacle = 0
     for i in range(8):
-        obstacle+=observation.distances[0].distance
-        print(observation.distances[0].detected,observation.distances[0].distance)
-
-    if(obstacle==0):
-        command.linear = 0.01
+        obstacle += observation.distances[i].distance
+        print(i, observation.distances[i].detected, observation.distances[i].distance)
+    obs = [i for i, d in enumerate(observation.distances) if d.detected and d.distance < 0.1]
+    global last_turn
+    if 2 not in obs and 3 not in obs:  # FORWARD
+        command.linear = 0.05
         command.angular = 0.0
+        command.LED = 'green'
+    elif 0 not in obs or 1 not in obs:
+        command.linear = 0.0
+        last_turn = command.angular = 0.2
+        command.LED = 'blue'
+    elif 5 not in obs or 4 not in obs:
+        command.linear = 0.0
+        last_turn = command.angular = -0.2
+        command.LED = 'blue'
+    elif 7 not in obs or 6 not in obs:
+        command.linear = 0.0
+        command.angular = last_turn
+        command.LED = 'blue'
     else:
         command.linear = 0.0
-        command.angular = 0.1
+        command.angular = last_turn
+        command.LED = 'red'
