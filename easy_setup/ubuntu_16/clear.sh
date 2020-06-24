7ce8a52426272febe8b372770ea10784374e4211
Amirreza Kabiri <amirezakabiri@gmail.com>
2020-06-24 19:45:38 +0430

    Update clear.sh

--- easy_setup/ubuntu_16/clear.sh
+++ easy_setup/ubuntu_16/clear.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
 pid=$(lsof -ti tcp:8080)
-if [[ $pid ]]; then
+if [ $pid ]; then
   kill -9 $pid
 fi
 
 pid=$(lsof -ti tcp:4719)
-if [[ $pid ]]; then
+if [ $pid ]; then
   kill -9 $pid
 fi
 
